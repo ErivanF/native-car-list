9c5dbf7b1e57bd75c06955d093e34c353bcedd56
ErivanF <erivan.c39@gmail.com>
2025-01-15 11:15:04 -0300

    build:add react-navigation lib

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import "./global.css";
+import { NavigationContainer } from "@react-navigation/native";
 // App.js
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-700">
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-red-700">
+          Open up App.tsx to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
