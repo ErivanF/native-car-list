6f79b45642600ed76951cf80e2d109a354aeca31
ErivanF <erivan.c39@gmail.com>
2025-01-15 10:31:51 -0300

    chore:set up nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./source/**/*.tsx"],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["App.tsx", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
