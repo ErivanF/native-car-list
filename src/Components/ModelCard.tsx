84de5649dcd733a10c603982b64eec74d4263be4
ErivanF <erivan.c39@gmail.com>
2025-01-17 10:42:22 -0300

    feat: model card

--- src/Components/ModelCard.tsx
+++ src/Components/ModelCard.tsx
@@ -4,7 +4,7 @@ import { IModel } from "../Types";
 interface Props {
   model: IModel;
 }
-export default function BrandCard({ model }: Props) {
+export default function ModelCard({ model }: Props) {
   return (
     <View className="flex flex-row items-center justify-between m-2 p-2 w-10-12 bg-white rounded-md border-purple-900 border-2">
       <Text className="text-purple-800 text-xl">{model.nome}</Text>
